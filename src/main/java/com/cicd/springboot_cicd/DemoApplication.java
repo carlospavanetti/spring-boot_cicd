be67ce0e11081d5a3e9e5145f7c640d277575923
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-11-10 10:46:36 -0300

    Add home endpoint

--- src/main/java/com/cicd/springboot_cicd/DemoApplication.java
+++ src/main/java/com/cicd/springboot_cicd/DemoApplication.java
@@ -2,9 +2,17 @@ package com.cicd.springboot_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello, devops";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
